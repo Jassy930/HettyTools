b89d8487146cdb2ba32600a516dd9e93df950ba9
Jassy <fengw930@163.com>
2019-05-06 22:50:09 +0800

    bug fix (CW) && ico

--- HettyTools/HettyTools/HamburgerMenuRipple/Handle_Compiler_Warnings.xaml.cs
+++ HettyTools/HettyTools/HamburgerMenuRipple/Handle_Compiler_Warnings.xaml.cs
@@ -25,46 +25,24 @@ namespace MetroDemo
             StreamWriter sw = File.CreateText(outfile);
 
             int flag = 0;
+            string branch33 = string.Empty;
+            sw.WriteLine("The following warnings are not on the list of acceptable warnings"); sw.WriteLine("");
             while (!sr.EndOfStream)
             {
                 string line = sr.ReadLine();
-                if (flag == 0) { sw.WriteLine("The following warnings are not on the list of acceptable warnings"); sw.WriteLine(""); flag++; }
-                if (flag == 1)
+                if (!Regex.IsMatch(line, "1643|1479|1518|1606|1522|1517|1516|1604|1772") && Regex.IsMatch(line, "dcc:") && !Regex.IsMatch(line, "1004|1025|1481|1500|1551|1552|1554|1555|1607|1608") && Regex.IsMatch(line, vob))
                 {
-                    if (line.Contains("CRITICAL WARNING SUMMARY"))
-                    {
-                        flag++;
-                        sw.WriteLine("");
-                        sw.WriteLine("");
-                        sw.WriteLine("The following warnings have been converted to errors as of Branch 33");
-                        sw.WriteLine("");
-                    }
-                    else
-                    {
-                        string no = line.Split(' ')[0];
-                        if (!Regex.IsMatch(no, "1643|1479|1518|1606|1522|1517|1516|1604|1772") && Regex.IsMatch(line, "dcc:") && !Regex.IsMatch(no, "1004|1025|1481|1500|1551|1552|1554|1555|1607|1608") && Regex.IsMatch(line, vob))
-                        {
-                            sw.WriteLine(line);
-                        }
-                    }
+                    sw.WriteLine(line);
                 }
-                if (flag == 2)
-                {
-                    if (Regex.IsMatch(line, "dcc:1004|dcc:1025|dcc:1481|dcc:1500|dcc:1551|dcc:1552|dcc:1554|dcc:1555|dcc:1607|dcc:1608"))
-                    {
-                        sw.WriteLine(line);
-                    }
-                }
+
+                if (Regex.IsMatch(line, "dcc:1004|dcc:1025|dcc:1481|dcc:1500|dcc:1551|dcc:1552|dcc:1554|dcc:1555|dcc:1607|dcc:1608"))
+                    branch33 += System.Environment.NewLine + line;
             }
+            sw.WriteLine("\n\nThe following warnings have been converted to errors as of Branch 33\n");
+            sw.Write(branch33);
 
             sr.Close();
             sw.Close();
-
-            //Console.WriteLine("Done...");
-            //Console.WriteLine("Wish you a happy day~~~");
-
-            //Console.ReadLine();
-            
         }
 
         private void Grid_Drop(object sender, System.Windows.DragEventArgs e)
